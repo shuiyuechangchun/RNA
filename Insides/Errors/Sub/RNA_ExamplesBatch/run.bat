ea827e76d476355153d5c6d859827e322d787787
谢特 <599513282@qq.com>
2021-02-10 16:08:49 +0800

    update Insides/Errors/Sub/RNA_ExamplesBatch/run.bat.

--- Insides/Errors/Sub/RNA_ExamplesBatch/run.bat
+++ Insides/Errors/Sub/RNA_ExamplesBatch/run.bat
@@ -3,40 +3,40 @@ SetLocal EnableExtensions EnableDelayedExpansion
 
 REM     Batch   Windows(CR LF) ANSI
 
-
+REM 接收R.N.A.EXE传入参数%1：工程目录
+REM 设置变量
 Set PROJECT_DIR=%1
 Set LOCAL_DIR=%cd%
 Set BATCH_DIR=%~dp0
 
 
 Cls
-echo ����Ŀ¼���·����!PROJECT_DIR!
-echo ����Ŀ¼����·����!LOCAL_DIR!\!PROJECT_DIR!
-echo ����Ŀ¼����·����!LOCAL_DIR!
-echo ��ǰ�������·����!BATCH_DIR!
+echo 工程目录相对路径：!PROJECT_DIR!
+echo 工程目录绝对路径：!LOCAL_DIR!\!PROJECT_DIR!
+echo 工具目录绝对路径：!LOCAL_DIR!
+echo 当前插件绝对路径：!BATCH_DIR!
 
 Pause
 
-
 Cls
-echo ����Ŀ¼����·����!LOCAL_DIR!
+echo 工具目录绝对路径：!LOCAL_DIR!
+REM 打印文件夹内文件/夹
 Dir
 Pause
 
+REM 切入!PROJECT_DIR!
 Pushd !PROJECT_DIR!
 Cls
-echo ����Ŀ¼���·����!PROJECT_DIR!
+echo 工程目录相对路径：!PROJECT_DIR!
+REM 打印文件夹内文件/夹
 Dir
 Pause
 
-::Move /y fakefs !label! 1>Nul 2>Nul
-::Del /s /q /f 1.exe 1>Nul 2>Nul
-::Del /s /q /f 2.txt 1>Nul 2>Nul
-
-
+REM 切回原路径
 Popd
 Cls
-echo ����Ŀ¼����·����!LOCAL_DIR!
+echo 工具目录绝对路径：!LOCAL_DIR!
+REM 打印文件夹内文件/夹
 Dir
 Pause
 Exit
